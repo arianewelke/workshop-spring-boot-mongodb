76f21480cb9a5088e276e135b3afb99868f5207d
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-22 21:08:41 -0300

    Obtendo um usuário por id

--- src/main/java/com/arianewelke/workshopmongo/services/UserService.java
+++ src/main/java/com/arianewelke/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.arianewelke.workshopmongo.services;
 
 import com.arianewelke.workshopmongo.domain.User;
 import com.arianewelke.workshopmongo.repository.UserRepository;
+import com.arianewelke.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,11 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
+
+
