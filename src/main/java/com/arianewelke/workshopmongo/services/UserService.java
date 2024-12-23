dcb192511c03fe38d204d3ca77638f65e9abb4e3
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-22 21:22:53 -0300

    Inserção de usuário com POST

--- src/main/java/com/arianewelke/workshopmongo/services/UserService.java
+++ src/main/java/com/arianewelke/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.arianewelke.workshopmongo.services;
 
 import com.arianewelke.workshopmongo.domain.User;
+import com.arianewelke.workshopmongo.dto.UserDTO;
 import com.arianewelke.workshopmongo.repository.UserRepository;
 import com.arianewelke.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
 
