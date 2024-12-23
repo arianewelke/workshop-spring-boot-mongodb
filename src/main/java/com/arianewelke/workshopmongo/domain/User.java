6ae1c5a5237fb83ccf104e35d0258096386fa4ab
Ariane Wwelke <arianewelkep97@gmail.com>
2024-12-22 22:54:27 -0300

    Referenciando os posts do usuário

--- src/main/java/com/arianewelke/workshopmongo/domain/User.java
+++ src/main/java/com/arianewelke/workshopmongo/domain/User.java
@@ -1,18 +1,24 @@
 package com.arianewelke.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
 public class User implements Serializable {
 
-    @Id private String id;
+    @Id
+    private String id;
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -45,6 +51,14 @@ public class User implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
